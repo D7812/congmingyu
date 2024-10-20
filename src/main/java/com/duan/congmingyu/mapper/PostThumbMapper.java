d4d9f06c1c6b1c54cf46fab7699ea7342505ac27
佩奇 <1335807812@qq.com>
2024-10-21 00:25:51 +0800

    1.初始化后端项目模板

---
+++ src/main/java/com/duan/congmingyu/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.duan.congmingyu.mapper;
+
+import com.duan.congmingyu.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
